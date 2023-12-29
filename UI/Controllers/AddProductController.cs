a036fc45123b35deb1813d4f41d04affd8c1f820
Grisha Rovniy <rovniygrishaaa@gmail.com>
2023-12-29 22:34:17 +0200

    create addproduct controller

---
+++ UI/Controllers/AddProductController.cs
@@ -0,0 +1,26 @@
+﻿using UI;
+using CourseWork.Product;
+
+namespace CourseWork.UI
+{
+    public class AddProductController : IControllerService
+    {
+        private DataBase.DataBase Data { get; }
+
+        public AddProductController(DataBase.DataBase dataBase)
+        {
+            Data = dataBase;
+        }
+
+        public string PrintMessage()
+        {
+            return " 1. Add products to the store";
+        }
+
+        public void DoAction()
+        {
+            var ps = new ProductService(Data);
+            ps.AddProduct();
+        }
+    }
+}
