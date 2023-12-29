01d0c847f1489ea7c5ee60435a9b0f6b2c495a28
Grisha Rovniy <rovniygrishaaa@gmail.com>
2023-12-29 22:36:37 +0200

    create buyproduct controller

---
+++ UI/Controllers/BuyProductController.cs
@@ -0,0 +1,28 @@
+﻿using CourseWork.Purchase;
+using UI;
+
+namespace CourseWork.UI
+{
+    public class BuyProductController : IControllerService
+    {
+        private DataBase.DataBase Data { get; }
+        private User.User User { get; }
+
+        public BuyProductController(DataBase.DataBase dataBase, User.User user)
+        {
+            Data = dataBase;
+            User = user;
+        }
+
+        public string PrintMessage()
+        {
+            return " 2. Buy products";
+        }
+
+        public void DoAction()
+        {
+            var ps = new PurchaseService(Data);
+            ps.Buy(User);
+        }
+    }
+}
