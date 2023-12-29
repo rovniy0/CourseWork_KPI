ec24f02e6e7ba5d5484d2577d7c76c153f637b11
Grisha Rovniy <rovniygrishaaa@gmail.com>
2023-12-29 22:41:01 +0200

    create database system

---
+++ DataBase/DataBase.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace CourseWork.DataBase
+{
+    public class DataBase  // TODO make static?
+    {
+        public List<User.User> Users { get; }
+        public List<Product.Product> Products { get; }
+
+        public DataBase()
+        {
+            Users = new List<User.User>();
+            Products = new List<Product.Product>();
+        }
+    }
+}
---
+++ DataBase/ParserDB.cs
@@ -0,0 +1,40 @@
+﻿using System.IO;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+
+namespace CourseWork.DataBase
+{
+    public class ParseInfo
+    {
+        private string _filePath =
+            @"C:\Users\Юзер\Desktop\Online-shop\DataBase\Files\";
+
+        public void SaveAllProductsToDb(DataBase dataBase)
+        {
+            var path = string.Concat(_filePath, "Products.json");
+            var serialized = JsonConvert.SerializeObject(dataBase.Products);
+            File.WriteAllText(path, serialized);
+        }
+
+        public void SaveAllUsersToDb(DataBase dataBase)
+        {
+            var path = string.Concat(_filePath, "Users.json");
+            var serialized = JsonConvert.SerializeObject(dataBase.Users);
+            File.WriteAllText(path, serialized);
+        }
+
+        public List<Product.Product> ReadProductsFromDb()
+        {
+            var path = string.Concat(_filePath, "Products.json");
+            var products = File.ReadAllText(path);
+            return JsonConvert.DeserializeObject<List<Product.Product>>(products);
+        }
+
+        public List<User.User> ReadUsersFromDb()
+        {
+            var path = string.Concat(_filePath, "Users.json");
+            var users = File.ReadAllText(path);
+            return JsonConvert.DeserializeObject<List<User.User>>(users);
+        }
+    }
+}
