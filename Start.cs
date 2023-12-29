b16e1283d0e1e9abc94e50dd8d1c3945aff6cf78
Grisha Rovniy <rovniygrishaaa@gmail.com>
2023-12-29 22:30:52 +0200

    create project

---
+++ Start.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using CourseWork.UI;
+
+namespace CourseWork
+{
+    internal class Start
+    {
+        public static void Main(string[] args)
+        {
+        Console.WriteLine("=====================================");
+        Console.WriteLine("\t Welcome to the store\t");
+        Console.WriteLine("=====================================\n");
+        var newController = new MainController();
+        newController.RunProgram();
+        }
+    }
+}
