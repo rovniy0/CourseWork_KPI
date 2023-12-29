00630f3a5bc1647e3ca0fce158e04c12cdf8b484
Grisha Rovniy <rovniygrishaaa@gmail.com>
2023-12-29 22:37:20 +0200

    create show product controller

---
+++ UI/Controllers/ShowProductController.cs
@@ -0,0 +1,26 @@
+﻿using CourseWork.Product;
+using UI;
+
+namespace CourseWork.UI
+{
+    public class ShowProductController : IControllerService
+    {
+        private DataBase.DataBase Db { get; }
+
+        public ShowProductController(DataBase.DataBase dataBase)
+        {
+            Db = dataBase;
+        }
+
+        public string PrintMessage()
+        {
+            return " 4. Show available products";
+        }
+
+        public void DoAction() 
+        {
+            var ps = new ProductService(Db);
+            ps.ShowProducts();
+        }
+    }
+}
